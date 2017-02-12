3a141eade3b7bc2a4a35e5be67bc40a6d9e12f55
Denis Beliaev <cimmwolf@gmail.com>
2017-02-12 20:37:37 +0700

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var flexibility = require('postcss-flexibility');
+var postCss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssNano = require('cssnano');
+var cache = require('gulp-cached');
+
+// Components
+var polyClean = require('polyclean');
+var htmlMin = require('gulp-htmlmin');
+var postCssHtml = require('gulp-html-postcss');
+var gulpHtmlEntities = require('gulp-html-entities');
+
+gulp.task('default', ['sass', 'components'],function() {
+    gulp.src('dist/css/*.css')
+        .pipe(postCss([
+            flexibility,
+            autoprefixer({browsers: ['last 3 versions'], cascade: false}),
+            cssNano({safe: true})
+        ]))
+        .pipe(gulp.dest('dist/css'));
+    gulp.src([
+        'bower_components/iron-*/*.html',
+        'bower_components/paper-*/*.html',
+        'bower_components/google-*/*.html',
+        'bower_components/gold-*/*.html',
+        'bower_components/neon-*/*.html',
+        'bower_components/platinum-*/*.html',
+        'bower_components/polymer/*.html'
+    ], {base: 'bower_components'})
+        .pipe(cache('components'))
+        .pipe(polyClean.cleanCss())
+        .pipe(polyClean.leftAlignJs())
+        .pipe(polyClean.uglifyJs())
+        .pipe(htmlMin({removeComments: true}))
+        .pipe(gulp.dest('bower_components'));
+    return gulp.src('dist/components/*')
+        .pipe(polyClean.uglifyJs())
+        .pipe(htmlMin({removeComments: true}))
+        .pipe(postCssHtml([
+            flexibility,
+            autoprefixer({browsers: ['last 3 versions'], cascade: false}),
+            cssNano({safe: true})
+        ]))
+        .pipe(gulpHtmlEntities('decode'))
+        .pipe(gulp.dest('dist/components'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(['src/sass/*.sass'])
+        .pipe(sass({includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']}).on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('components', function () {
+    return gulp.src('src/components/*')
+        .pipe(gulp.dest('dist/components'));
+});
