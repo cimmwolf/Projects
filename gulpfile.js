766e85962591c0ea75f18bdbd82f3c5e53cd894c
Denis Beliaev <cimmwolf@gmail.com>
2017-09-29 17:11:21 +0300

    Completely change structure of app

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var flexibility = require('postcss-flexibility');
 var postCss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssNano = require('cssnano');
 var cache = require('gulp-cached');
 
 // Components
-var polyClean = require('polyclean');
 var htmlMin = require('gulp-htmlmin');
 var postCssHtml = require('gulp-html-postcss');
-var gulpHtmlEntities = require('gulp-html-entities');
 
 gulp.task('default', ['sass', 'components'], function () {
     gulp.src('dist/css/*.css')
         .pipe(postCss([
-            flexibility,
-            autoprefixer({browsers: ['> 1%', 'last 3 versions'], cascade: false}),
+            autoprefixer(),
             cssNano({safe: true})
         ]))
         .pipe(gulp.dest('dist/css'));
@@ -30,20 +26,26 @@ gulp.task('default', ['sass', 'components'], function () {
         'bower_components/polymer/*.html'
     ], {base: 'bower_components'})
         .pipe(cache('components'))
-        .pipe(polyClean.cleanCss())
-        .pipe(polyClean.leftAlignJs())
-        .pipe(polyClean.uglifyJs())
-        .pipe(htmlMin({removeComments: true}))
+        .pipe(htmlMin({
+            removeComments: true,
+            preventAttributesEscaping: true,
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
         .pipe(gulp.dest('bower_components'));
     return gulp.src('dist/components/*')
-        .pipe(polyClean.uglifyJs())
-        .pipe(htmlMin({removeComments: true}))
+        .pipe(htmlMin({
+            removeComments: true,
+            preventAttributesEscaping: true,
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true
+        }))
         .pipe(postCssHtml([
-            flexibility,
-            autoprefixer({browsers: ['> 1%', 'last 3 versions'], cascade: false}),
+            autoprefixer(),
             cssNano({safe: true})
         ]))
-        .pipe(gulpHtmlEntities('decode'))
         .pipe(gulp.dest('dist/components'));
 });
 
