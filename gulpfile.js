28c7b40ed0202ddc82d39449f5fd69fc9d9e5412
Denis Beliaev <cimmwolf@gmail.com>
2017-11-08 14:25:11 +0300

    Moved to Yarn

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,14 @@ var postCssHtml = require('gulp-html-postcss');
 
 gulp.task('compress_components', function () {
     return gulp.src([
-        'bower_components/iron-*/*.html',
-        'bower_components/paper-*/*.html',
-        'bower_components/google-*/*.html',
-        'bower_components/gold-*/*.html',
-        'bower_components/neon-*/*.html',
-        'bower_components/platinum-*/*.html',
-        'bower_components/polymer/*.html'
-    ], {base: 'bower_components'})
+        'node_modules/@bower_components/iron-*/*.html',
+        'node_modules/@bower_components/paper-*/*.html',
+        'node_modules/@bower_components/google-*/*.html',
+        'node_modules/@bower_components/gold-*/*.html',
+        'node_modules/@bower_components/neon-*/*.html',
+        'node_modules/@bower_components/platinum-*/*.html',
+        'node_modules/@bower_components/polymer/*.html'
+    ], {base: 'node_modules/@bower_components'})
         .pipe(cache('components'))
         .pipe(htmlMin({
             removeComments: true,
@@ -25,7 +25,7 @@ gulp.task('compress_components', function () {
             minifyJS: true,
             minifyCSS: true
         }))
-        .pipe(gulp.dest('bower_components'));
+        .pipe(gulp.dest('node_modules/@bower_components'));
 });
 
 gulp.task('publish', ['compress_components'], function () {
